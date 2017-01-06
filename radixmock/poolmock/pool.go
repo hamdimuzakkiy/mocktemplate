c1727664f5b529d7ada56145e09efc79decee12e
muuzakiiy <hamdiahmadi1504@gmail.com>
2017-01-06 18:58:57 +0700

    add pool mock

---
+++ radixmock/poolmock/pool.go
@@ -0,0 +1,16 @@
+package poolmock
+
+import (
+	"github.com/mediocregopher/radix.v2/redis"
+	"github.com/stretchr/testify/mock"
+)
+
+type PoolTemplate struct {
+	mock.Mock
+}
+
+func (p PoolTemplate) Cmd(cmd string, args ...interface{}) *redis.Resp {
+	argsMock := p.Called(cmd, args)
+
+	return argsMock.Get(0).(*redis.Resp)
+}
