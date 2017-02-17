16c6ee52eba168867bb51e027e26ffdf2d83a359
muuzakiiy <hamdiahmadi1504@gmail.com>
2017-02-17 14:24:00 +0700

    update Cmd to cmds

--- radixmock/poolmock/pool.go
+++ radixmock/poolmock/pool.go
@@ -9,7 +9,7 @@ type PoolTemplate struct {
 	mock.Mock
 }
 
-func (p PoolTemplate) Cmd(cmd string, args ...interface{}) *redis.Resp {
+func (p PoolTemplate) Cmds(cmd string, args ...interface{}) *redis.Resp {
 	argsMock := p.Called(cmd, args)
 
 	return argsMock.Get(0).(*redis.Resp)
